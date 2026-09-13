3b0b9cdacaffd024946d550c027a88ab2d721898
Максим <rene@MacBook-Air-Maksim.local>
2026-09-13 15:59:45 +0800

    fixed function create_cart

--- src/cart/cart_repository.rs
+++ src/cart/cart_repository.rs
@@ -8,19 +8,17 @@ use crate::cart_models::{CartItem, CreateCartItem};
 // GET    /cart             получить корзину
 // PATCH  /cart/items/:id   изменить quantity
 // DELETE /cart/items/:id   удалить товар
-pub async fn create_cart(pool:PgPool,cart_item: CreateCartItem)->Result<CartItem, sqlx::Error>{
+pub async fn create_cart(pool:&PgPool,cart_item: CreateCartItem)->Result<CartItem, sqlx::Error>{
     sqlx::query_as!(
         CartItem,
         r#"
-        INSERT INTO cart_items(user_id,product_id,quantity,created_at,updated_at)
-        VALUES ($1,$2,$3,$4,$5)
-        RETURNING id, name, description, price, stock,category_id
+        INSERT INTO cart_items(user_id,product_id,quantity)
+        VALUES ($1,$2,$3)
+        RETURNING id,user_id, product_id, quantity, created_at, updated_at
         "#,
         cart_item.user_id,
         cart_item.product_id,
         cart_item.quantity,
-        cart_item.created_at,
-        cart_item.updated_at
 
     )
         .fetch_one(pool).await
